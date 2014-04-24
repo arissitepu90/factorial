6c5a17274fce8a4782f18eecdd06a739df2f388f
arissitepu90 <arissitepu15@gmail.com>
2014-04-24 18:20:34 +0700

    onex

---
+++ Factorial.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package factorial;
+import java.util.Scanner;
+ 
+class Factorial
+{
+   public static void main(String args[])
+   {
+      int n, c, fact = 1;
+ 
+      System.out.println("Input : ");
+      Scanner in = new Scanner(System.in);
+ 
+      n = in.nextInt();
+ 
+      if ( n < 0 )
+         System.out.println("Tidak Boleh negatif.");
+      else
+      {
+         for ( c = 1 ; c <= n ; c++ )
+            fact = fact*c;
+ 
+         System.out.println("Hasil "+n+"= "+fact);
+      }
+   }
+}
